9d5f084f600ebfe77662e59527a308eb0d09cfbc
Crab <kbudrin@yandex.ru>
2019-01-28 14:40:00 +0500

    Done

---
+++ CalcProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalcProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите выражение: ");
+                    Console.WriteLine(ReversPolishNotation.Calculate(Console.ReadLine()));
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте еще раз");
+                }
+            }
+        }
+    }
+}
